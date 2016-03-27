4f21e7e66d63fce5440760e1877233b039b4e7ad
eom5a <encoenda007>
2016-03-27 19:54:24 +0200

    User resource added x2

--- service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterMediaType.java
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterMediaType.java
@@ -4,4 +4,9 @@ package edu.upc.eetac.dsa.beeter;
  * Created by Enric on 27/03/2016.
  */
 public class BeeterMediaType {
+    public final static String BEETER_AUTH_TOKEN = "application/vnd.dsa.beeter.auth-token+json";
+    public final static String BEETER_USER = "application/vnd.dsa.beeter.user+json";
+    public final static String BEETER_STING = "application/vnd.dsa.beeter.sting+json";
+    public final static String BEETER_STING_COLLECTION = "application/vnd.dsa.beeter.sting.collection+json";
+    public final static String BEETER_ROOT = "application/vnd.dsa.beeter.root+json";
 }
--- service/src/main/java/edu/upc/eetac/dsa/beeter/WebApplicationExceptionMapper.java
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/WebApplicationExceptionMapper.java
@@ -1,7 +1,21 @@
 package edu.upc.eetac.dsa.beeter;
 
+import edu.upc.eetac.dsa.beeter.entity.BeeterError;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
 /**
  * Created by Enric on 27/03/2016.
  */
-public class WebApplicationExceptionMapper {
+@Provider
+public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+    @Override
+    public Response toResponse(WebApplicationException e) {
+        BeeterError error = new BeeterError(e.getResponse().getStatus(), e.getMessage());
+        return Response.status(error.getStatus()).entity(error).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
 }
